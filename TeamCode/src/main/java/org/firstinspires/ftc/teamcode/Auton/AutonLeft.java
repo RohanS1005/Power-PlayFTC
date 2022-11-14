2c81e61c83da05bd2523187929aff943b7fcd1ba
jaimanm <93413974+jaimanm@users.noreply.github.com>
2022-11-14 08:21:26 -0500

    updated auton files- added plan comments

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton/AutonLeft.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.Auton;
+
+public class AutonLeft {
+  // first path:
+  // 1. Scan signal cone
+  // 2. Turn and place preloaded cone on low junction
+  // 3. Turn and pick up signal cone
+  // 4. Move forward and place signal cone on high junction
+  // 5. Move back to starting point
+  // 6. Park in spot according to signal cone
+
+  // second path:
+  // 1. Scan signal cone
+  // 2. Strafe right and drive forward and place preloaded cone on high junction
+  // 3. Move left and back and pick up signal cone facing the wall
+  // 4. Turn around and move forward and place signal cone on high junction
+  // 5. Come back to starting point
+  // 6. Park in spot according to signal cone
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton/AutonRight.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.Auton;
+
+public class AutonRight {
+  // first path
+  // 1. scan signal cone
+  // 2. Turn left and place preloaded cone on low junction
+  // 3. Turn back forward and pick up signal cone
+  // 4. Drive forwards and turn left and place signal cone on high junction
+  // 5. Turn around and come back to starting point
+  // 6. Park in spot indicated by the color sensor reading
+
+  // second path
+  // 1. scan signal cone
+  // 2. Strafe left, drive forward, and place preloaded cone on high junction
+  // 3. Turn right, Turn right again, drive forward, and pick up signal cone
+  // 4. Turn around, drive forward, and place signal cone on high junction
+  // 5. Turn around, go back to starting point
+  // 6. Park in spot indicated by the color sensor reading
+}
