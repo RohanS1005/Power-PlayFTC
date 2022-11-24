6e99d55e303cc38aaf5b1f66ddee6a3dc4f07dec
Rohan Dugad <rohand2468@gmail.com>
2022-11-24 18:17:56 -0500

    Made pole presets

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/FinalTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/FinalTeleOp.java
@@ -9,26 +9,27 @@ import org.firstinspires.ftc.teamcode.Drivers._Drivetrain;
 @TeleOp(group="FinalTeleOp")
 public class FinalTeleOp extends _TeleOp {
 
+    private final int _LOW_POLE = 6;
+    private final int _MID_POLE = 14;
+    private final int _HIGH_POLE = 24;
+
+    private boolean _triggerPressed = false;
+    private int _height = 0;
+
     @Override
     public void init() {
         Robot.setup(hardwareMap, telemetry, Robot.SetupType.TeleOp1);
-        Robot.setFieldSide(Robot.FieldSide.RED);
-    }
-
-    @Override
-    public void init_loop() {
     }
 
     @Override
     public void start() {
         Robot.setup(hardwareMap, telemetry, Robot.SetupType.TeleOp2);
-        Robot.getIMU().willUpdate(true);
     }
 
     @Override
     public void loop() {
         Robot.update();
-        telemetry.addLine("Encoder Counts: " + Robot.getLinearslide().getCounts());
+        Robot.telemetry.addLine("" + Robot.getLinearslide().isBusy());
 
         if(gamepad1.left_stick_x!=0 || gamepad1.left_stick_y!=0){
             double left_stick_y = -gamepad1.left_stick_y;
@@ -58,23 +59,37 @@ public class FinalTeleOp extends _TeleOp {
         //left trigger up and right trigger down
         // assume encoder count increase as linear slide moves up
         //change 5
-        if (gamepad2.left_trigger != 0 ) { //&& Robot.getLinearslide().getCounts()/Robot.getLinearslide().getCountsPerInch() >0
-            Robot.getLinearslide().runSpeed(0.02);
+        if (gamepad2.right_trigger != 0) { //&& Robot.getLinearslide().getCounts()/Robot.getLinearslide().getCountsPerInch() >0
+            _triggerPressed = true;
+            Robot.getLinearslide().runSpeed(0.2);
         }
-        else if (gamepad2.right_trigger != 0  ) {//&& Robot.getLinearslide().getCounts()/Robot.getLinearslide().getCountsPerInch()<95
-            Robot.getLinearslide().runSpeed(-0.3);
+        else if (gamepad2.left_trigger != 0) {//&& Robot.getLinearslide().getCounts()/Robot.getLinearslide().getCountsPerInch()<95
+            _triggerPressed = true;
+            Robot.getLinearslide().runSpeed(-0.2);
         }
-        else if (gamepad2.dpad_left){
-        }
-        else if (gamepad2.dpad_up){
-        }
-        else if (gamepad2.dpad_left){
-        }
-        else if (gamepad2.dpad_down){
-        }
-        else {
+        else if (_triggerPressed) {
+            _triggerPressed = false;
             Robot.getLinearslide().stop();
         }
+        else {
+            if (gamepad2.dpad_left) {
+                Robot.getLinearslide().runDistance(0.5, _LOW_POLE);
+                _height = _LOW_POLE;
+            }
+            else if (gamepad2.dpad_up){
+                Robot.getLinearslide().runDistance(0.5, _MID_POLE);
+                _height = _MID_POLE;
+            }
+            else if (gamepad2.dpad_right){
+                Robot.getLinearslide().runDistance(0.5, _HIGH_POLE);
+                _height = _HIGH_POLE;
+            }
+            else if (gamepad2.dpad_down){
+                Robot.getLinearslide().runDistance(0.5, -_height);
+                _height = 0;
+            }
+        }
+
         if (gamepad2.y){
             //assume that 0 is up position
             Robot.getClaw6().setSlowPosition(0, 5);
